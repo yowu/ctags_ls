621104777f7f446a22a47126cdd85bad4dea36f2
Yong Wu <gnoywu@gmail.com>
2025-12-14 23:52:24 +0800

    Add url crate and use it for URI/path conversions in utils.

--- src/utils.rs
+++ src/utils.rs
@@ -1,46 +1,27 @@
 use std::{io, path::PathBuf};
 
 use lsp_types::Uri;
+use url::Url;
 
-/// Utility functions for handling URI and path conversions
 pub struct UriUtils;
 
 impl UriUtils {
-    pub fn file_path_to_uri<P: AsRef<str>>(path: P) -> io::Result<Uri> {
-        let uri_string = format!("file://{}", path.as_ref());
-        uri_string
+    /// Convert a file path to a URI using url::Url::from_file_path
+    pub fn file_path_to_uri<P: AsRef<std::path::Path>>(path: P) -> io::Result<Uri> {
+        let url = Url::from_file_path(path)
+            .map_err(|_| io::Error::other("Failed to convert path to URI"))?;
+
+        url.as_str()
             .parse()
             .map_err(|e| io::Error::other(format!("Failed to parse URI: {}", e)))
     }
 
+    /// Convert a URI to a file path using url::Url::to_file_path
     pub fn uri_to_file_path(uri: &Uri) -> io::Result<PathBuf> {
-        let uri_str = uri.as_str();
-        if let Some(stripped) = uri_str.strip_prefix("file://") {
-            Ok(PathBuf::from(stripped))
-        } else {
-            Err(io::Error::other(format!(
-                "URI is not a file URI: {}",
-                uri_str
-            )))
-        }
-    }
-}
+        let url = Url::parse(uri.as_str())
+            .map_err(|e| io::Error::other(format!("Failed to parse URI: {}", e)))?;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_file_path_to_uri() {
-        let path = "/home/user/test.txt";
-        let uri = UriUtils::file_path_to_uri(path).unwrap();
-        assert_eq!(uri.as_str(), "file:///home/user/test.txt");
-    }
-
-    #[test]
-    fn test_uri_to_file_path() {
-        let uri: Uri = "file:///home/user/test.txt".parse().unwrap();
-        let path = UriUtils::uri_to_file_path(&uri).unwrap();
-        assert_eq!(path, PathBuf::from("/home/user/test.txt"));
+        url.to_file_path()
+            .map_err(|_| io::Error::other("Failed to convert URI to file path"))
     }
 }
